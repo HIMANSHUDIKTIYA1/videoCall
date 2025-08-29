119f1dfa40e896a4546f0068967ff7d3fe1c3027
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2025-08-29 22:01:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const server = createServer(app);
 const port = process.env.Port || 3000;
 const io = new Server(server ,{
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://hd-room.netlify.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true
